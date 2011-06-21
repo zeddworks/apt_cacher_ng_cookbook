eafea4ef8e88a9c075fb0c2bd848ceb5a399cd58
Scott McLeod <halcyonblue@gmail.com>
2011-06-21 16:39:03 -0400

    License under Apache 2

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "ZeddWorks"
-maintainer_email "halcyonblue@gmail.com"
-license          "All rights reserved"
+maintainer_email "smcleod@zeddworks.com"
+license          "Apache 2.0"
 description      "Installs/Configures apt-cacher-ng"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.2"
